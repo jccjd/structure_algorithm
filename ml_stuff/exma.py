0057ac0bd4bd8c565b8d4018361c36a2471bc578
jccjd <1340944538@qq.com>
2022-01-14 21:05:35 -1000

    ss

--- ml_stuff/exma.py
+++ ml_stuff/exma.py
@@ -1,40 +1,93 @@
-import xlsxwriter                     #导入模块
- 
-workbook = xlsxwriter.Workbook('new_excel.xlsx')   #创建新的excel
- 
-worksheet = workbook.add_worksheet('sheet1')        #创建新的sheet
- 
-headings = ['Number','testA','testB']             #创建表头
- 
-data = [
-    ['2017-9-1','2017-9-2','2017-9-3','2017-9-4','2017-9-5','2017-9-6'],
-    [10,40,50,20,10,50],
-    [30,60,70,50,40,30],
-]                                                       #自己造的数据
- 
-worksheet.write_row('A1',headings)
- 
-worksheet.write_column('A2',data[0])
-worksheet.write_column('B2',data[1])
-worksheet.write_column('C2',data[2])                    #将数据插入到表格中
- 
-chart_col = workbook.add_chart({'type':'line'})        #新建图表格式 line为折线图
-chart_col.add_series(                                   #给图表设置格式，填充内容
-    {
-        'name':'=sheet1!$B$1',
-        'categories':'=sheet1!$A$2:$A$7',
-        'values':   '=sheet1!$B$2:$B$7',
-        'line': {'color': 'red'},
-        'data_labels': {'value': True} #显示数字，就是直方图上面的数字，默认不显示
-    }
-)
- 
-chart_col.set_title({'name':'测试'})
-chart_col.set_x_axis({'name':"x轴"})
-chart_col.set_y_axis({'name':'y轴'})          #设置图表表头及坐标轴
- 
-chart_col.set_style(1)
- 
-worksheet.insert_chart('A10',chart_col,{'x_offset':25,'y_offset':10})   #放置图表位置
- 
-workbook.close()
+import datetime
+import sys
+
+import pandas as pd
+import argparse
+
+import xlsxwriter
+
+
+def my_map(x):
+    return x.strftime('%H:%M:%S')
+
+
+def time_sq(rg_length, send=1):
+    """
+    :input [1, 2, 3]
+    :return 一个时间序列
+    :example:
+        input: 3,2
+        output:[datetime.datetime(1900, 1, 1, 0, 0),
+        datetime.datetime(1900, 1, 1, 0, 0, 2),
+        datetime.datetime(1900, 1, 1, 0, 0, 4)]
+
+    """
+    pd_date = pd.date_range("2021-1-1", periods=rg_length, freq=f'{send}s')
+    return [datetime.datetime.strptime(i.strftime('%H:%M:%S'), '%H:%M:%S') for i in pd_date]
+
+
+def hello(file_name, sends):
+    # 构造数据
+    list2 = []
+    for lin2 in open(file_name):
+        if lin2.startswith('[SUM]'):
+            sw_str = lin2.strip('[SUM]')
+            list1 = sw_str.split(' ')
+            #
+            # list2.append(list1[3])
+            # list2.append(list1[6])
+            # list2.append(list1[9])
+            list2.append(float(list1[-2]))
+
+    list2_len = len(list2)
+
+    pd_date = time_sq(list2_len, sends)
+    workbook = xlsxwriter.Workbook(file_name.split('.')[0] + '.xlsx')  # 创建新的excel
+
+    worksheet = workbook.add_worksheet('sheet1')  # 创建新的sheet
+
+    headings = ['Time', 'Data']  # 创建表头
+    data = [
+        pd_date,
+        list2,
+
+    ]
+    time_formats = workbook.add_format({'num_format': 'hh:mm:ss'})
+    worksheet.write_row("A1", headings)
+    worksheet.write_column("A2", data[0], time_formats)
+    worksheet.write_column("B2", data[1])
+    chart_col = workbook.add_chart({'type': 'scatter'})
+
+    x = f'=Sheet1!$A$2:$A${len(list2)}'
+    y = f'=Sheet1!$B$2:$B${len(list2)}'
+
+    chart_col.set_size({'width': 1500,
+                        'height': 500})
+    chart_col.add_series(
+        {
+            "name": '=sheet1!$B$1',
+            "categories": x,
+            "values": y,
+            'marker': {'type': 'circle',
+                       'size': 3,
+                       'border': {'color': 'blue'},
+                       'fill':   {'color': 'blue'}
+                       },
+        }
+    )
+    chart_col.set_title({'name': 'Date 点状图'})
+    chart_col.set_x_axis({'name': "time"})
+    chart_col.set_y_axis({'name': "gb"})
+
+    chart_col.set_style(1)
+    worksheet.insert_chart("D4", chart_col, {'x_offset': 25, 'y_offset': 10})
+    workbook.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="iperf 数据转表格")
+    parser.add_argument('--path', type=str, help='指定处理的文件路径')
+    parser.add_argument('--sec', type=int, help='iperf 打流的时间间隔 默认为1')
+
+    args = parser.parse_args()
+    hello(args.path, args.sec)
